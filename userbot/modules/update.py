55c0f524ef4b82dfba7c0a277f0c0a373769e18f
i-am-cool-guy <felixuserbot@gmail.com>
2024-07-30 20:57:43 +0530

    Update update.py

--- userbot/modules/update.py
+++ userbot/modules/update.py
@@ -1,12 +1,20 @@
 from telethon import events
 from userbot import Neo
 from userbot.utils import update, lang
+import json
+
 LANG = {
   'UPDATE_INFO': 'Updates Neo to latest version.',
+  'ROLLBACK_INFO': 'Rollbacks Neo to previous version.',
+  'ROLLBACK_NONE': '**Please specify a version to rollback!**\n__For instance, 1.0, 1.1, 1.2 ...__',
   'UPDATING': '`Updating...`',
+  'ROLLING_BACK': '`Rolling back to v{}`',
   'UP-TO-DATE': '**Neo is up-to-date.**',
+  'NO_VERSION': '**No such version to rollback.**',
   'UPDATE_FAILED': '**Failed to update Neo!**',
-  'UPDATED': '**Neo is updated.**'
+  'ROLLBACK_FAILED': '**Failed to rollback Neo!**',
+  'UPDATED': '**Neo is updated.**',
+  'ROLLED_BACK': '**Neo rolled back to v{}.**'
 }
 
 @Neo.command(
@@ -27,3 +35,25 @@ async def _update(event):
   if status == False:
     return await event.edit(LANG['UP-TO-DATE'])
   return await event.reply(LANG['UPDATED'])
+
+@Neo.command(
+  pattern='^rollback ?(.*)',
+  info=LANG['ROLLBACK_INFO']
+)
+async def _rollback(event):
+  text = event.pattern_match.group(1) or None
+  if text == None:
+    return await event.edit(LANG['ROLLBACK_NONE'])
+  versions_list = []
+  with open('../versions.json', 'r') as f:
+    Json = f.read()
+    Json = json.load(Json)
+
+  try: 
+    status = await rollback(version)
+  except ValueError:
+    return await event.edit(LANG['ROLLBACK_FAILED'])
+  await event.edit(LANG['ROLLING_BACK'])
+  if status == False:
+    return await event.edit(LANG['ROLLING_FAILED'])
+  return await event.reply(LANG['ROLLED_BACK'])
