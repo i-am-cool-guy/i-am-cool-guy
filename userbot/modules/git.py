8f539becab2b7e94fea7b836f2126793a8a802d1
i-am-cool-guy <felixuserbot@gmail.com>
2024-07-30 21:43:36 +0530

    Update and rename update.py to git.py

--- userbot/modules/git.py
+++ userbot/modules/git.py
@@ -1,21 +1,8 @@
 from telethon import events
-from userbot import Neo
+from userbot import Neo, PREFIX
 from userbot.utils import update, rollback, lang
 
-LANG = {
-  'UPDATE_INFO': 'Updates Neo to latest version.',
-  'ROLLBACK_INFO': 'Rollbacks Neo to previous version.',
-  'ROLLBACK_NONE': '**Please specify a version to rollback!**\n__For instance, 1.0, 1.1, 1.2 ...__',
-  'UPDATING': '`Updating...`',
-  'ROLLING_BACK': '`Rolling back to v{}`',
-  'UP-TO-DATE': '**Neo is up-to-date.**',
-  'NO_VERSION': '**No such version to rollback.**',
-  'VERSION_DEP': '**Rolling back to v{} is no longer possible.**',
-  'UPDATE_FAILED': '**Failed to update Neo!**',
-  'ROLLBACK_FAILED': '**Failed to rollback Neo!**',
-  'UPDATED': '**Neo is updated.**',
-  'ROLLED_BACK': '**Neo rolled back to v{}.**'
-}
+LANG = lang('git')
 
 @Neo.command(
   pattern='^update ?(.*)',
@@ -24,9 +11,9 @@ LANG = {
 async def _update(event):
   text = event.pattern_match.group(1) or None
   if text == None or text != 'now':
-    return await event.edit(
-      await update(True)
-    )
+    changelog = await update(True)
+    if changelog:
+      return await event.edit(LANG['CHANGELOG'].format(changelog, PREFIX))
   try: 
     status = await update(False)
   except ValueError:
@@ -44,12 +31,13 @@ async def _rollback(event):
   text = version = event.pattern_match.group(1) or None
   if text == None:
     return await event.edit(LANG['ROLLBACK_NONE'])
-
+  if version == VERSION:
+    return await event.edit(LANG['CURRENT_VER'].format(version))
   try: 
     status = await rollback(version)
   except ValueError:
     return await event.edit(LANG['ROLLBACK_FAILED'])
-  await event.edit(LANG['ROLLING_BACK'])
+  await event.edit(LANG['ROLLING_BACK'].format(version))
   if status == False:
     return await event.edit(LANG['NO_VERSION'])
-  return await event.reply(LANG['ROLLED_BACK'])
+  return await event.reply(LANG['ROLLED_BACK'].format(version))
