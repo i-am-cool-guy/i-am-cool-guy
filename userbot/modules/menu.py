2d005893b350d21d73a12aafab2a68623bca6423
i-am-cool-guy <felixuserbot@gmail.com>
2024-08-30 22:32:21 +0530

    Update menu.py

--- userbot/modules/menu.py
+++ userbot/modules/menu.py
@@ -10,22 +10,31 @@ LANG = lang('menu')
 )
 async def menu(event):
   menu = ''
-  for command in COMMANDS:
-    hide = command.get('hide', False)
-    if not hide:
-      cmd = command.get('command', False)
-      info = command.get('info', False)
-      usage = command.get('usage', False)
-      example = command.get('example', False)
-      if cmd and info:
-        menu += f'**{LANG["COMMAND"]}:** `{PREFIX[0]}{cmd}`\n'
-        menu += f'**{LANG["INFO"]}:** `{info}`\n'
+  text = event.match.group(0) or False
+  if text == False:
+    for command in COMMANDS:
+      hide = command.get('hide', False)
+      if not hide:
+        cmd = command.get('command', False)
+        info = command.get('info', False)
+        if cmd and info:
+          menu += f'**{PREFIX[0]}{cmd}**\n'
+          menu += f'__{info}__\n\n'
+  else:
+    for command in COMMANDS:
+      if text == command and command.get('hide', False) == True: 
+        cmd = command.get('command', False)
+        info = command.get('info', False)
+        usage = command.get('usage', False)
+        example = command.get('example', False)
+        menu += f'**{cmd.upper()}**\n'
+        menu += f'__{info}__\n\n'
         if usage:
-          menu += f'**{LANG["USAGE"]}:** `{usage}`\n'
+          menu += f'```{usage}```'
         if example:
-          menu += f'**{LANG["EXAMPLE"]}:** `{example}`\n'
-        menu += '\n'
-  if menu:
+          menu += f'```{example}```'
+        
+  if menu != '':
     await event.edit(menu.strip())
   else:
     await event.edit(f'**{LANG["MENU_FAILED"]}**')
