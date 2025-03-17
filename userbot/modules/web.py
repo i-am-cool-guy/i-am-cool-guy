4a8d4a3ddd3f4ab1aeb5f7f718e2c2bd1481e627
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-17 17:19:27 +0530

    Update web.py

--- userbot/modules/web.py
+++ userbot/modules/web.py
@@ -14,21 +14,21 @@ async def dict(event):
     return await event.edit(LANG['DEF_NONE'])
   await event.edit(LANG['SEARCH'])
   data = await request('get', 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text, 'json')
-  print(data)
-  if data[0]:
-    word = data[0]['word']
-    phonetics = data[0]['phonetics'][0]['text'] if data[0]['phonetics'] else ''
-    parts_of_speech = data[0]['meanings'][0]['partOfSpeech']
-    definition = data[0]['meanings'][0]['definitions'][0]['definition']
-    example = next((item.get('example', '') for item in data[0]['meanings'][0]['definitions'] if 'example' in item), '')
-    rtext = (
-      f"{LANG['WORD']} : **{word}**\n"
-      f"{LANG['PHONETICS']} : **{phonetics}**\n"
-      f"{LANG['POP']} : **{parts_of_speech}**\n"
-      f"{LANG['DEF']} :\n**{definition}**"
-    )
-    if example:
-      rtext += f"\n__{lang('menu')['EXAMPLE']}__ : **{example}**"
-      await event.reply(rtext)
-  else:
-    await event.reply(LANG['DEF_FAILED'])
+  try:
+    if data[0]:
+      word = data[0]['word']
+      phonetics = data[0]['phonetics'][0]['text'] if data[0]['phonetics'] else ''
+      parts_of_speech = data[0]['meanings'][0]['partOfSpeech']
+      definition = data[0]['meanings'][0]['definitions'][0]['definition']
+      example = next((item.get('example', '') for item in data[0]['meanings'][0]['definitions'] if 'example' in item), '')
+      rtext = (
+        f"{LANG['WORD']} : **{word}**\n"
+        f"{LANG['PHONETICS']} : **{phonetics}**\n"
+        f"{LANG['POP']} : **{parts_of_speech}**\n"
+        f"{LANG['DEF']} :\n**{definition}**"
+      )
+      if example:
+        rtext += f"\n__{lang('menu')['EXAMPLE']}__ : **{example}**"
+        await event.reply(rtext)
+  except KeyError:
+      await event.reply(LANG['DEF_FAILED'])
