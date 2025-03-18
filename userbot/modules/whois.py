884fa0d18c27d297ac41762dfa11f7146f826d6b
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-18 16:15:49 +0530

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -9,7 +9,7 @@ LANG = lang('whois')
   info='Get information of the user.'
 )
 async def whois(event):
-  user = Neo.me.id
+  user = (await client.get_me()).id
   if event.is_reply:
     replied_msg = await event.get_reply_message()
     user = (await replied_msg.get_sender()).id
