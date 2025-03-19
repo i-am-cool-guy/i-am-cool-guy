bcf1fec384ced750b38d7f7b26e40dd6db083c2c
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-19 19:38:37 +0530

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -18,8 +18,9 @@ async def whois(event):
       if isinstance(entity, events.message.MessageEntityMentionName):
         user = entity.user_id
   user_info = await Neo.get_entity(user)
+  print(user_info)
   pp = await Neo.download_profile_photo(user, file=os.path.join("../temp/", f"{user}.jpg"))
-  info = f"User ID: {user_info.id}\nUsername: {user_info.username}\nFirst Name: {user_info.first_name}\n{f'Last Name: {user_info.last_name}' if user_info.last_name else ''}{f'Phone: {user_info.phone}\n' if user_info.phone else ''}Is Bot: {'Yes' if user_info.bot else 'No'}"
+  info = f"User ID: {user_info.id}\nUsername: {user_info.username}\nFirst Name: {user_info.first_name}\n{f'Last Name: {user_info.last_name}' if user_info.last_name else ''}Is Bot: {'Yes' if user_info.bot else 'No'}"
   if pp:
     await Neo.send_file(event.chat_id, f"../temp/{user}.jpg", caption=info)
   else:
