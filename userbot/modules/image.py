b70abe9d126e95e04158da268236850c3910630f
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-20 14:09:39 +0530

    Create image.py

---
+++ userbot/modules/image.py
@@ -0,0 +1,18 @@
+from telethon import events 
+from google_images_download import google_images_download
+from userbot import Neo
+from userbot.utils import lang
+import os
+LANG = lang('image')
+
+@Neo.command(
+  pattern='^image ?(.*)',
+  info='Download google images for your query.'
+)
+async def image(event):
+  if event.pattern_match.group(0):
+    response = google_images_download.googleimagesdownload()
+    images = response.download({ "keywords": f"{event.pattern_match.group(0)} -sa", "limit": 10 }) 
+    print(images)
+  else:
+    await event.reply("Cannot find images for your query!")
