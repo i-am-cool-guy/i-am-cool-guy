10774f2a0cd7ee17653862e6be7865dc2b48a1ab
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-21 17:06:07 +0530

    Update image.py

--- userbot/modules/image.py
+++ userbot/modules/image.py
@@ -1,28 +1,28 @@
 from telethon import events 
-from google_images_downloader import GoogleImagesDownloader
+from simple_image_download import simple_image_download as sid
 from userbot import Neo
 from userbot.utils import lang
 import os
+
 LANG = lang('image')
 
 @Neo.command(
-  pattern='^image ?(.*)',
-  info='Download google images for your query.'
+    pattern='^image ?(.*)',
+    info='Download Google images for your query.'
 )
 async def image(event):
-  text = event.pattern_match.group(1) or False
-  if text == False:
-    return await event.reply('**Please enter some keywords to search!**')
-  downloader = GoogleImagesDownloader(browser="chrome", show=False, debug=False,
-                                    quiet=False, disable_safeui=False)
-  downloader.download(text, limit=10, destination="../temp/", file_format="jpg")
-  downloader.close()
-  images = []
-  for image in os.listdir("../temp/"):
-    if image.endsWith("jpg") or image.endsWith("png") or image.endsWith("jpeg"):
-      images.append(os.path.join("../temp/", image))
-  if not images:
-    return await event.reply('**No images found.**')
-  for path in images:
-    await Neo.send_file(event.chat_id, path)
-    os.remove(image_path)
+    text = event.pattern_match.group(1) or False
+    if text == False:
+        return await event.reply('**Please enter some keywords to search!**')
+    downloader = sid.Downloader()
+    download_path = "../temp/"
+    downloader.download(text, limit=10, extensions={".jpg", ".png", ".jpeg"}, output_directory=download_path)
+    images = []
+    for image in os.listdir(download_path):
+        if image.endswith(("jpg", "png", "jpeg")):
+            images.append(os.path.join(download_path, image))
+    if not images:
+        return await event.reply('**No images found.**')
+    for image_path in images:
+        await Neo.send_file(event.chat_id, image_path)
+        os.remove(image_path)
