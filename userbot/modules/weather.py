4dac9087a97879a636038bcef04b1b336e1e1efc
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-26 18:03:01 +0530

    Update weather.py

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -1,6 +1,6 @@
 from telethon import events 
 from userbot import Neo
-from userbot.utils import lang
+from userbot.utils import lang, request
 import os
 
 LANG = lang('weather')
@@ -13,3 +13,5 @@ async def weather(event):
     text = event.pattern_match.group(1) or False
     if not text:
         return await event.reply('**Please enter any place name!**')
+    json = await request('GET', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en")
+    
