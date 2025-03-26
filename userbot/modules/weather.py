c3befade6fdcf2fe6ab6f323192d4e575ecd52d3
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-26 18:06:36 +0530

    Update weather.py

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -13,5 +13,14 @@ async def weather(event):
     text = event.pattern_match.group(1) or False
     if not text:
         return await event.reply('**Please enter any place name!**')
-    json = await request('GET', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en")
-    
+    data = await request('GET', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en")
+    await event.reply(f"""_City_ : **${data.name}*"
+__Weather__ : **${data.weather[0].main}**
+__Climate__ : **${data.weather[0].description}**
+__Temperature__ : **${data.main.temp}°C**
+__Pressure__ : **${data.main.pressure} hPa**
+__Humidity Level__ : **${data.main.humidity}%**
+__Visibility__ : **${data.visibility} meters**
+__Wind Speed__ : **${data.wind.speed} m/s**
+__Wind Direction__ : **${data.wind.deg}°**
+__Cloudiness__ : **${data.clouds.all}%**""")
