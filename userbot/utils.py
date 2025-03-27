cfbc17e8f0614383d34cd627425d36a73741ba8f
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-27 11:28:35 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -142,7 +142,7 @@ async def request(method, url, res):
     async with getattr(session, method)(url, headers={
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }) as response:
-      return (response.json() if res == 'json' else response.text()) or False
+      return (await response.json() if res == 'json' else await response.text()) or False
 
 def translate(text, lang_to=LANGUAGE):
   translator = Translator()
