21b43060f5c558449a6ea5bf0ba380f120229f16
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-31 21:56:51 +0530

    Update weather.py

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -1,6 +1,8 @@
 from telethon import events 
 from userbot import Neo
 from userbot.utils import lang, request
+from aiocache import Cache
+import asyncio
 import os
 
 LANG = lang('weather')
@@ -13,7 +15,16 @@ async def weather(event):
     text = event.pattern_match.group(1) or False
     if not text:
         return await event.reply(LANG['WEATHER_NONE'])
-    data = await request('get', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en", 'json')
-    await event.reply(
-        LANG['WEATHER'].format(data["name"], data["weather"][0]["main"], data["weather"][0]["description"], data["main"]["temp"], data["main"]["pressure"], data["main"]["humidity"], data["visibility"], data["wind"]["speed"], data["wind"]["deg"], data["clouds"]["all"])
-    )
+    cache = Cache(Cache.MEMORY)
+    with asyncio.Runner() as runner:
+      data = runner.run(cache.get(text))
+      if data:
+        return await event.reply(
+          LANG['WEATHER'].format(data["name"], data["weather"][0]["main"], data["weather"][0]["description"], data["main"]["temp"], data["main"]["pressure"], data["main"]["humidity"], data["visibility"], data["wind"]["speed"], data["wind"]["deg"], data["clouds"]["all"])
+        )
+      else:
+        data = await request('get', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en", 'json')
+        runner.run(cache.set(text, data))
+        return await event.reply(
+          LANG['WEATHER'].format(data["name"], data["weather"][0]["main"], data["weather"][0]["description"], data["main"]["temp"], data["main"]["pressure"], data["main"]["humidity"], data["visibility"], data["wind"]["speed"], data["wind"]["deg"], data["clouds"]["all"])
+        )
