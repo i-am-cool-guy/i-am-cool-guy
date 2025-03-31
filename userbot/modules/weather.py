a6ae13656ee1fd353aa1c57e9c2160d613e363ff
i-am-cool-guy <felixuserbot@gmail.com>
2025-03-31 22:19:55 +0530

    Update weather.py

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -13,20 +13,24 @@ LANG = lang('weather')
     info=LANG['WEATHER_INFO']
 )
 async def weather(event):
-    text = event.pattern_match.group(1) or False
-    if not text:
-        return await event.reply(LANG['WEATHER_NONE'])
-    cache = Cache(Cache.MEMORY)
-    with asyncio.Runner() as runner:
-      data = runner.run(cache.get(text))
-      if data:
-        data = json.loads(data)
-        return await event.reply(
-          LANG['WEATHER'].format(data["name"], data["weather"][0]["main"], data["weather"][0]["description"], data["main"]["temp"], data["main"]["pressure"], data["main"]["humidity"], data["visibility"], data["wind"]["speed"], data["wind"]["deg"], data["clouds"]["all"])
-        )
-      else:
-        data = await request('get', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en", 'json')
-        runner.run(cache.set(text, json.dumps(data)))
-        return await event.reply(
-          LANG['WEATHER'].format(data["name"], data["weather"][0]["main"], data["weather"][0]["description"], data["main"]["temp"], data["main"]["pressure"], data["main"]["humidity"], data["visibility"], data["wind"]["speed"], data["wind"]["deg"], data["clouds"]["all"])
-        )
+  text = event.pattern_match.group(1) or False
+  if not text:
+    return await event.reply(LANG['WEATHER_NONE'])
+    
+  cache = Cache(Cache.MEMORY)
+  data = await cache.get(text)
+  if data:
+    data = json.loads(data)
+  else:
+    data = await request('get', f"http://api.openweathermap.org/data/2.5/weather?q={text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en", 'json')
+    await cache.set(text, json.dumps(data))
+
+  return await event.reply(
+    LANG['WEATHER'].format(
+      data["name"], data["weather"][0]["main"], 
+      data["weather"][0]["description"], data["main"]["temp"], 
+      data["main"]["pressure"], data["main"]["humidity"], 
+      data["visibility"], data["wind"]["speed"], 
+      data["wind"]["deg"], data["clouds"]["all"]
+    )
+  )
