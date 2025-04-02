a3f00e19c198758a2353d49f2adc904e0590ee51
i-am-cool-guy <felixuserbot@gmail.com>
2025-04-02 13:40:37 +0530

    Update group.py

--- userbot/modules/group.py
+++ userbot/modules/group.py
@@ -1,6 +1,7 @@
 from userbot import Neo
 from userbot.utils import lang
-from telethon.tl.functions.channels import EditBannedRequest, EditAdminRequest, InviteToChannelRequest, ExportChatInviteRequest
+from telethon.tl.functions.channels import EditBannedRequest, EditAdminRequest, InviteToChannelRequest
+from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.types import ChatBannedRights, ChatAdminRights
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsAdmins
