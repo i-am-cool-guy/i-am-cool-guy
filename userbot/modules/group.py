260af4bab4d96412f0951a0fd823d4552b7433b1
i-am-cool-guy <felixuserbot@gmail.com>
2025-04-02 14:04:02 +0530

    Update group.py

--- userbot/modules/group.py
+++ userbot/modules/group.py
@@ -2,150 +2,155 @@ from userbot import Neo
 from userbot.utils import lang
 from telethon.tl.functions.channels import EditBannedRequest, EditAdminRequest, InviteToChannelRequest
 from telethon.tl.functions.messages import ExportChatInviteRequest
-from telethon.tl.types import ChatBannedRights, ChatAdminRights, ChannelParticipantsAdmins
+from telethon.tl.types import ChatBannedRights, ChatAdminRights
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsAdmins
 from datetime import timedelta
 import re
 
 LANG = lang('group')
 
 def parse_time(time_str):
-  match = re.match(r"(\d+)([smhd])", time_str)
-  if match:
-    value, unit = int(match[1]), match[2]
-    if unit == "s":
-      return timedelta(seconds=value)
-    elif unit == "m":
-      return timedelta(minutes=value)
-    elif unit == "h":
-      return timedelta(hours=value)
-    elif unit == "d":
-      return timedelta(days=value)
-  return None
+    if not time_str:
+        return None
+    match = re.match(r"(\d+)([smhd])", time_str)
+    if match:
+        value, unit = int(match[1]), match[2]
+        return {
+            "s": timedelta(seconds=value),
+            "m": timedelta(minutes=value),
+            "h": timedelta(hours=value),
+            "d": timedelta(days=value),
+        }.get(unit)
+    return None
 
 async def is_admin(event):
-  user = await Neo.get_me()
-  admins = await Neo.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
-  if user.id not in [admin.id for admin in admins]:
-    await event.edit(LANG['NOT_ADMIN'])
-    return False
-  return True
+    user = await event.client.get_me()
+    admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+    if user.id not in [admin.id for admin in admins]:
+        await event.edit(LANG['NOT_ADMIN'])
+        return False
+    return True
 
 async def get_user(event):
-  if event.is_reply:
-    return (await event.get_reply_message()).sender_id
-  user = event.pattern_match.group(1)
-  return await Neo.get_entity(user) if user else None
+    if event.is_reply:
+        return (await event.get_reply_message()).sender_id
+    user = event.pattern_match.group(1)
+    return await Neo.get_entity(user) if user else None
 
 @Neo.command(
-  pattern='^mute ?(.*)',
-  info=LANG['MUTE_INFO'],
-  usage='.mute <user> <duration>',
-  example='.mute @username 10m'
+    pattern=r'^mute (\S+)\s*(\d+[smhd]?)?',
+    info=LANG['MUTE_INFO'],
+    usage='.mute <user> <duration (optional)>',
+    example='.mute @username 10m'
 )
 async def mute(event):
-  if not await is_admin(event):
-    return
-  user = await get_user(event)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  duration = parse_time(event.pattern_match.group(2))
-  rights = ChatBannedRights(until_date=duration, send_messages=True)
-  await Neo(EditBannedRequest(event.chat_id, user, rights))
-  await event.edit(LANG['MUTED'].format(user))
+    if not await is_admin(event):
+        return
+    
+    user_input = event.pattern_match.group(1)
+    duration_input = event.pattern_match.group(2)
+    
+    user = await Neo.get_entity(user_input)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    duration = parse_time(duration_input)
+    rights = ChatBannedRights(until_date=duration, send_messages=True)
+    
+    await event.client(EditBannedRequest(event.chat_id, user.id, rights))
+    mute_type = f"for {duration_input}" if duration_input else "permanently"
+    await event.edit(f"Muted {user.username or user.id} {mute_type}.")
 
 @Neo.command(
-  pattern='^unmute ?(.*)',
-  info=LANG['UNMUTE_INFO'],
-  usage='.unmute <user>',
-  example='.unmute @username'
+    pattern='^unmute (\S+)',
+    info=LANG['UNMUTE_INFO'],
+    usage='.unmute <user>',
+    example='.unmute @username'
 )
 async def unmute(event):
-  if not await is_admin(event):
-    return
-  user = await get_user(event)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  rights = ChatBannedRights(send_messages=False)
-  await Neo(EditBannedRequest(event.chat_id, user, rights))
-  await event.edit(LANG['UNMUTED'].format(user))
+    if not await is_admin(event):
+        return
+    
+    user = await get_user(event)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    rights = ChatBannedRights(send_messages=False)
+    await event.client(EditBannedRequest(event.chat_id, user, rights))
+    await event.edit(LANG['UNMUTED'].format(user))
 
 @Neo.command(
-  pattern='^ban ?(.*)',
-  info=LANG['BAN_INFO'],
-  usage='.ban <user>',
-  example='.ban @username'
+    pattern='^ban (\S+)',
+    info=LANG['BAN_INFO'],
+    usage='.ban <user>',
+    example='.ban @username'
 )
 async def ban(event):
-  if not await is_admin(event):
-    return
-  user = await get_user(event)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  rights = ChatBannedRights(until_date=None, view_messages=True)
-  await Neo(EditBannedRequest(event.chat_id, user, rights))
-  await event.edit(LANG['BANNED'].format(user))
+    if not await is_admin(event):
+        return
+    
+    user = await get_user(event)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    rights = ChatBannedRights(until_date=None, view_messages=True)
+    await event.client(EditBannedRequest(event.chat_id, user, rights))
+    await event.edit(LANG['BANNED'].format(user))
 
 @Neo.command(
-  pattern='^kick ?(.*)',
-  info=LANG['KICK_INFO'],
-  usage='.kick <user>',
-  example='.kick @username'
+    pattern='^kick (\S+)',
+    info=LANG['KICK_INFO'],
+    usage='.kick <user>',
+    example='.kick @username'
 )
 async def kick(event):
-  if not await is_admin(event):
-    return
-  user = await get_user(event)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  rights = ChatBannedRights(until_date=None, view_messages=True)
-  await Neo(EditBannedRequest(event.chat_id, user, rights))
-  await Neo(EditBannedRequest(event.chat_id, user, ChatBannedRights(view_messages=False)))
-  await event.edit(LANG['KICKED'].format(user))
+    if not await is_admin(event):
+        return
+    
+    user = await get_user(event)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    rights = ChatBannedRights(until_date=None, view_messages=True)
+    await event.client(EditBannedRequest(event.chat_id, user, rights))
+    await event.client(EditBannedRequest(event.chat_id, user, ChatBannedRights(view_messages=False)))
+    await event.edit(LANG['KICKED'].format(user))
 
 @Neo.command(
-  pattern='^add ?(.*)',
-  info=LANG['ADD_INFO'],
-  usage='.add <user>',
-  example='.add @username'
+    pattern='^add (\S+)',
+    info=LANG['ADD_INFO'],
+    usage='.add <user>',
+    example='.add @username'
 )
 async def add(event):
-  if not await is_admin(event):
-    return
-  user = event.pattern_match.group(1)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  await Neo(InviteToChannelRequest(event.chat_id, [user]))
-  await event.edit(LANG['ADDED'].format(user))
+    if not await is_admin(event):
+        return
+    
+    user = event.pattern_match.group(1)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    await event.client(InviteToChannelRequest(event.chat_id, [user]))
+    await event.edit(LANG['ADDED'].format(user))
 
 @Neo.command(
-  pattern='^promote ?(.*)',
-  info=LANG['PROMOTE_INFO'],
-  usage='.promote <user> <permissions>',
-  example='.promote @username pin_messages'
+    pattern=r'^promote (\S+)\s*(\S+)?',
+    info=LANG['PROMOTE_INFO'],
+    usage='.promote <user> <permissions (optional)>',
+    example='.promote @username pin_messages'
 )
 async def promote(event):
-  if not await is_admin(event):
-    return
-  user = await get_user(event)
-  if not user:
-    return await event.edit(LANG['NO_USER'])
-  permissions = event.pattern_match.group(2)
-  rights = ChatAdminRights(**{permissions: True}) if permissions else ChatAdminRights(post_messages=True, add_admins=False, pin_messages=True)
-  await Neo(EditAdminRequest(event.chat_id, user, rights, LANG['PROMOTED']))
-  await event.edit(LANG['PROMOTED'].format(user))
-
-@Neo.command(
-  pattern='^restrict ?(.*)',
-  info=LANG['RESTRICT_INFO'],
-  usage='.restrict <type>',
-  example='.restrict poll'
-)
-async def restrict(event):
-  if not await is_admin(event):
-    return
-  restriction = event.pattern_match.group(1)
-  if not restriction:
-    return await event.edit(LANG['NO_TYPE'])
-  rights = ChatBannedRights(**{restriction: True})
-  await Neo(EditBannedRequest(event.chat_id, None, rights))
-  await event.edit(LANG['RESTRICTED'].format(restriction))
+    if not await is_admin(event):
+        return
+    
+    user_input = event.pattern_match.group(1)
+    permissions_input = event.pattern_match.group(2)
+    
+    user = await Neo.get_entity(user_input)
+    if not user:
+        return await event.edit(LANG['NO_USER'])
+    
+    rights = ChatAdminRights(**{permissions_input: True}) if permissions_input else ChatAdminRights(post_messages=True, add_admins=False, pin_messages=True)
+    await event.client(EditAdminRequest(event.chat_id, user.id, rights, LANG['PROMOTED']))
+    await event.edit(LANG['PROMOTED'].format(user))
