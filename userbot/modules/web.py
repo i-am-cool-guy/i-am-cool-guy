d9a977782d41c53b966ae7e0e4098d3c6db22ae2
i-am-cool-guy <felixuserbot@gmail.com>
2025-05-03 13:19:21 +0530

    Update web.py

--- userbot/modules/web.py
+++ userbot/modules/web.py
@@ -29,6 +29,23 @@ async def dict(event):
       )
       if example:
         rtext += f"\n__{lang('menu')['EXAMPLE']}__ : **{example}**"
-        await event.reply(rtext)
+        await event.edit(rtext)
   except KeyError:
-      await event.reply(LANG['DEF_FAILED'])
+      await event.edit(LANG['DEF_FAILED'])
+
+@Neo.command(
+    pattern="^wiki ?(.*)",
+    info=LANG["WIKI_INFO"],
+    usage=".wiki <article>",
+    example=".wiki AI"
+)
+async def wikipedia(event):
+  text = event.pattern_match.group(1)
+  if not text:
+    return event.edit(LANG["WIKI_NONE"])
+  data = await request('get', 'https://toxicdevilapi.vercel.app/search/wikipedia?lang=en&query=' + text, 'json')
+  if data["status_code"] == 200:
+    info = "**" + data["result"]["title"] "**\n\n__" + data["result"]["info"] + "__\n\n" + data["result"]["url"]
+    return event.edit(info)
+  else:
+    return event.edit(LANG["WIKI_FAILED"])
